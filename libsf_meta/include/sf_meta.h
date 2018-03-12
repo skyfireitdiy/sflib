4750138237e044c90e20890ba885b71d26b2f7ee
SkyFire <skyfireitdiy@hotmail.com>
2018-03-12 23:31:31 +0800

    为sf_meta增加namespace

--- libsf_meta/include/sf_meta.h
+++ libsf_meta/include/sf_meta.h
@@ -3,20 +3,32 @@
 #include <functional>
 #include <tuple>
 
-template<class T>struct sf_function_type_helper{};
-
-template<class T, class ... U>
-struct sf_function_type_helper<std::function<T(U...)>>
+namespace skyfire
 {
-    using return_type=T;
-    using param_type= std::tuple<U...>;
-};
 
-template<typename T> struct sf_bind_type_helper{};
+    template<class T>
+    struct sf_function_type_helper
+    {
+    };
 
-template<template<typename> class T,typename U,typename...V>
-struct sf_bind_type_helper<T<U(V...)>>
-{
-    using return_type = U;
-    using param_type = std::tuple<V...>;
-};
+    template<class T, class ... U>
+    struct sf_function_type_helper<std::function<T(U...)>>
+    {
+        using return_type=T;
+        using param_type= std::tuple<U...>;
+    };
+
+    template<typename T>
+    struct sf_bind_type_helper
+    {
+    };
+
+    template<template<typename> class T, typename U, typename...V>
+    struct sf_bind_type_helper<T<U(V...)>>
+    {
+        using return_type = U;
+        using param_type = std::tuple<V...>;
+    };
+
+
+}
