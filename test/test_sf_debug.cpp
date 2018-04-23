6c447df9df574eba2b618e6d00e15243c1628cf3
SkyFire <skyfireitdiy@hotmail.com>
2018-04-24 00:22:24 +0800

    1.修复sf_debug由于使用匿名命名空间引入的问题

--- test/test_sf_debug.cpp
+++ test/test_sf_debug.cpp
@@ -14,8 +14,8 @@ void output(const char *str)
 
 int main()
 {
-    sf_debug ddd;
-    sf_debug::set_output_debug_string_func(output);
+    sf_debug_t ddd;
+    sf_debug_t::set_output_debug_string_func(output);
     ddd.sf_track("666", 123);
     sf_log("123",8997);
 }
