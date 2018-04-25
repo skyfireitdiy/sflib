00a06c74b710d3add12fcaea3d797645d34cb592
SkyFire <skyfireitdiy@hotmail.com>
2018-04-26 01:12:46 +0800

    完善同步链式调用

--- libsf_chain_call/include/sf_chain_call.h
+++ libsf_chain_call/include/sf_chain_call.h
@@ -1,24 +1,68 @@
 #pragma once
 
 #include <functional>
-
-#include "sf_nocopy.h"
+#include <thread>
 
 namespace skyfire
 {
-    template <typename _Ret, typename _Param ...>
-    class sf_chain_call : public sf_nocopy<>
+    template <typename _Ret>
+    class sf_chain_call__
     {
+    private:
+
+        std::function<_Ret()> func__;
+
     public:
 
-        sf_chain_call(std::function<_Ret(_Param...)> func, _Param && ... param)
+        sf_chain_call__(std::function<_Ret()> func) :
+                func__(func)
         {
-
         };
 
+        template <typename _R, typename ... _Params>
+        sf_chain_call__<_R> then(std::function<_R(_Ret, _Params...)> func, _Params&& ... param) const
+        {
+            std::function<_Ret()> f = [=, &param ... ]() -> _R{
+                func(func__(), std::forward<_Params>(param) ...);
+            };
+            return sf_chain_call__<_R> (f);
+        }
 
-    private:
-        std::function<_Ret(_Param ...)> func__;
+        template <typename _R, typename ... _Params>
+        sf_chain_call__<_R> then(_R(func)(_Ret, _Params...), _Params&& ... param) const
+        {
+            std::function<_Ret()> f = [=, &param ... ]() -> _R{
+                func(func__(), std::forward<_Params>(param) ...);
+            };
+            return sf_chain_call__<_R> (f);
+        }
+
+
+        auto call() const
+        {
+            return func__();
+        }
 
     };
+
+
+
+
+
+
+
+    template <typename _Ret, typename ... _Args>
+    sf_chain_call__<_Ret> make_chain_call(std::function<_Ret(_Args...)> func, _Args &&... args)
+    {
+        std::function<_Ret()> f = std::bind(func, std::forward<_Args>(args) ... );
+        return sf_chain_call__(f);
+    };
+
+    template <typename _Ret, typename ... _Args>
+    sf_chain_call__<_Ret> make_chain_call(_Ret(func)(_Args...), _Args &&... args)
+    {
+        std::function<_Ret()> f = std::bind(func, std::forward<_Args>(args) ... );
+        return sf_chain_call__(f);
+    };
+
 }
---
+++ test/test_chain_call.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "sf_chain_call.h"
+
+
+using namespace skyfire;
+using namespace std;
+
+int sum(int a, int b)
+{
+    cout<<a<<" "<<b<<endl;
+    return a+b;
+}
+
+int append(int a)
+{
+    cout<<a<<endl;
+    return a+1;
+}
+
+int append_n(int a, int n)
+{
+    cout<<"append "<<n<<endl;
+    return a+n;
+}
+
+
+int main()
+{
+    auto p = make_chain_call(sum, 1, 1).then(append).then(append).then(append_n, 25);
+    p.call();
+}
