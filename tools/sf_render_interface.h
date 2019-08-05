3d6d3a0bb642d435efe8e3dfafbf7e4d740c95e0
skyfire <skyfireitdiy@hotmail.com>
2019-08-05 17:34:49 +0800

    删除database，修复render返回值

--- tools/sf_render_interface.h
+++ tools/sf_render_interface.h
@@ -7,6 +7,6 @@ namespace skyfire
     class sf_render_interface
     {
     public:
-        virtual render(const std::string& str, const sf_json &data) = 0;
+        virtual std::string render(const std::string& str, const sf_json &data) = 0;
     };
 }
