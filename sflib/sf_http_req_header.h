8db3ee3f0c3608dc9abb95bc346f27b08e849523
skyfire <skyfireitdiy@hotmail.com>
2020-10-24 22:11:41 +0800

    增加注释

--- sflib/sf_http_req_header.h
+++ sflib/sf_http_req_header.h
@@ -5,6 +5,10 @@
 #include "sf_stdc++.h"
 namespace skyfire {
 
+/**
+ * @brief http 请求header
+ * 
+ */
 class http_req_header : public http_header {
 
 private:
@@ -12,12 +16,33 @@ private:
     http_request_line request_line__;
 
 public:
+    /**
+     * @brief 获取cookies
+     * 
+     * @return std::unordered_map<std::string, std::string> cookies
+     */
     std::unordered_map<std::string, std::string> cookies() const;
 
+    /**
+     * @brief 转为字符串
+     * 
+     * @return std::string 转换结果
+     */
     std::string to_string() const;
 
+    /**
+     * @brief 增加cookies
+     * 
+     * @param key 键
+     * @param value 值 
+     */
     void add_cookies(const std::string& key, const std::string& value);
 
+    /**
+     * @brief 设置请求行  
+     * 
+     * @param request_line 请求行
+     */
     void set_request_line(const http_request_line& request_line);
 };
 }
